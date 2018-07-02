dbd771abe5a2a8ee21d50ed86654de93d3e9390d
jenniferlong8 <jlong8@bu.edu>
2018-07-02 01:00:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
 function addElementToBeginningOfArray(array,element) {
   var new_array = [element, ...array]
   return new_array
