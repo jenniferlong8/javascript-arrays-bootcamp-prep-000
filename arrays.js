895be38e1cc46365bba079140d5672e32cc1c2d3
jenniferlong8 <jlong8@bu.edu>
2018-07-02 00:51:21 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,29 @@
+function addElementToBeginningOfArray(array,element) {
+  var new_array = [element, ...array]
+  return new_array
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  var new_array = [...array,element]
+  return new_array
+}
+
+function destructivelyAddElementToEndOfArray(array,element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array,index) {
+  var element = array[index]
+  return element
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
